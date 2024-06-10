f3cfef4b7e0c2ee0865a36f8b3930c7fb3531b7f
Raphael Floresca <rpngfl@gmail.com>
2024-06-11 02:31:41 +0800

    fix: allow CORS

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,19 @@ import { ExerciseCircuitMappingsService } from './exercise-circuit-mappings/exer
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Seed the exercises
   const exercisesService = app.get(ExercisesService);
   await exercisesService.seedData();
 
-
+  // Seed the circuits
   const circuitsService = app.get(CircuitsService);
   await circuitsService.seedData();
 
-
+  // Seed the mappings
   const exerciseCircuitMappingsService = app.get(ExerciseCircuitMappingsService);
   await exerciseCircuitMappingsService.seedData();
 
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
